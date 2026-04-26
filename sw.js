fdad3528288538e32064320fcb6b23b258b20fa1
aberthe22 <aberthe22@gmail.com>
2026-04-26 19:48:24 +0200

    v14.3

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// Service Worker Elie — 20260426-10
+// Service Worker Elie — 20260426-11
 // Stratégie : Network First (réseau prioritaire, cache en fallback offline)
-const CACHE = 'elie-20260426-15';
+const CACHE = 'elie-20260426-16';
 
 self.addEventListener('install', e => {
   self.skipWaiting(); // prend le contrôle immédiatement
